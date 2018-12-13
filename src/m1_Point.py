d4d33d4132ffb2f2b0391d3c8327c45548e19634
brownme1 <brownme1@rose-hulman.edu>
2018-12-13 13:30:48 -0500

    Project 9

--- src/m1_Point.py
+++ src/m1_Point.py
@@ -52,6 +52,8 @@ class Point(object):
         self.x= x
         self.y= y
         self.moves= 0
+        self.startx = x
+        self.staty = y
 
     def __repr__(self):
         return "Point({},{})".format(self.x , self.y)
@@ -75,6 +77,9 @@ class Point(object):
     def get_distance_from(self,p):
         import math
         return math.sqrt((self.x- p.x)**2 +(self.y- p.y)**2)
+
+    def get_distance_from_start(self):
+        return self.
 
 def run_test_init():
     """
@@ -804,6 +809,37 @@ def run_test_get_distance_from_start():
         print('p2 from start to (1, 1), should be about 0.0')
         print('Actually is:', p2.get_distance_from_start())
     """
+
+    p1 = Point(20, 30)
+    p1.move_to(111, 222)
+    p1.move_by(10, 20)
+    p1.move_to(0, 0)
+    p1.move_to(21, 31)
+    print()
+    print('p1 from start to (21, 31), should be about 1.414')
+    print('Actually is:', p1.get_distance_from_start())
+
+    p1.move_by(29, 39)
+    print()
+    print('p1 from start to (50, 70), should be about 50.0')
+    print('Actually is:', p1.get_distance_from_start())
+
+    p2 = Point(1, 1)
+    print()
+    print('p2 from start to (1, 1), should be about 0.0')
+    print('Actually is:', p2.get_distance_from_start())
+
+    p2.move_to(11, 1)
+    print()
+    print('p2 from start to (11, 1), should be about 10.0')
+    print('Actually is:', p2.get_distance_from_start())
+
+    p2.move_to(999, 999)
+    p2.move_to(1, 1)
+    print()
+    print('p2 from start to (1, 1), should be about 0.0')
+    print('Actually is:', p2.get_distance_from_start())
+
     # -------------------------------------------------------------------------
     # TODO: 10.  Follow the same instructions as in _TODO_ 3 above,
     #    but for the  get_distance_from_START  method specified above.
